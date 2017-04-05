a2d96c6ee7dde41e3e6a1de9f8cec8d4f5e729b3
Michael Dyrynda <michael@dyrynda.com.au>
2017-04-05 10:17:19 +0000

    Apply fixes from StyleCI

--- tests/Feature/UuidTest.php
+++ tests/Feature/UuidTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Feature;
 
+use Tests\Post;
+use Tests\UncastPost;
 use PHPUnit_Framework_TestCase;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 use Illuminate\Database\Capsule\Manager;
-use Tests\Post;
-use Tests\UncastPost;
 
 class UuidTest extends PHPUnit_Framework_TestCase
 {
