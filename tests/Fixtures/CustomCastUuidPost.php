a9a8b750bfcea037e9e606fb76ccc91924075d4c
Travis Austin <travis.austin@mspintegrations.com>
2019-02-01 21:01:56 +0000

    Added missing file

---
+++ tests/Fixtures/CustomCastUuidPost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Fixtures;
+
+class CustomCastUuidPost extends Model
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $casts = ['custom_uuid' => 'uuid'];
+    
+    public function uuidColumn()
+    {
+        return 'custom_uuid';
+    }
+}
